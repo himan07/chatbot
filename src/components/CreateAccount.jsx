619b326bc5f056f622a7651764cb78fe0ef5d63c
Himanshu Yadav <57978421+himan07@users.noreply.github.com>
2023-06-01 07:14:33 +0000

    changes

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -3,82 +3,67 @@ import "../assets/css/LoginPage.css";
 import { Button, TextField } from "@mui/material";
 
 const CreateAccount = (props) => {
-    const [email, setEmail] = useState([]);
-    const [password, setPassword] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
 
+  useEffect(() => {
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+  }, [email, password]);
 
-    useEffect(() => {
-        localStorage.setItem("email", email);
-        localStorage.setItem("password", password);
-    }, [email, password]);
+  const handleChangeEmail = (event) => {
+    let emaiValue = event.target.value;
+    setEmail(emaiValue);
+    localStorage.setItem("email", emaiValue);
+  };
 
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const handleChangeEmail = (event) => {
-        let emaiValue = event.target.value
-        setEmail(emaiValue)
-        localStorage.setItem("email", emaiValue);
-    };
+  const handleSubmit = () => {
+    props.dataFromCreateAccount(email, password);
+  };
 
-    const handleChangePassword = (event) => {
-        setPassword(event.target.value);
-    };
-
-
-    const handleSubmit = () => {
-        props.dataFromCreateAccount(email, password);
-    };
-
-    return (
-        <div className="create_account">
-            <div className="account_create_content">
-                <h1>Create your Account</h1>
-                <div className="details">
-                    <TextField
-                        variant="outlined"
-                        fullWidth
-                        label="Enter your Name"
-                        style={{ marginTop: "20px" }}
-                    />
-                    <TextField
-                        variant="outlined"
-                        fullWidth
-                        label="Enter your Email"
-                        style={{ marginTop: "20px" }}
-                        type="email"
-                        onChange={handleChangeEmail}
-                    />
-                    <TextField
-                        variant="outlined"
-                        fullWidth
-                        label="Enter your Password"
-                        style={{ marginTop: "20px" }}
-                        onChange={handleChangePassword}
-                        type="password"
-                    />
-                    <TextField
-                        variant="outlined"
-                        fullWidth
-                        label="Enter your age"
-                        style={{ marginTop: "20px" }}
-                    />
-                    <TextField
-                        variant="outlined"
-                        fullWidth
-                        label="Enter your Address"
-                        style={{ marginTop: "20px" }}
-                    />
-                </div>
-            </div>
-            <Button
-                variant="contained"
-                onClick={handleSubmit}
-                type="submit"
-                id="create_button"
-            >
-                Create Account
-            </Button>
+  return (
+    <div className="create_account">
+      <div className="account_create_content">
+        <h1>Create your Account</h1>
+        <div className="details">
+          <TextField
+            variant="outlined"
+            fullWidth
+            label="Enter your Name"
+            style={{ marginTop: "20px" }}
+          />
+          <TextField
+            variant="outlined"
+            fullWidth
+            label="Enter your Email"
+            style={{ marginTop: "20px" }}
+            type="email"
+            onChange={handleChangeEmail}
+          />
+          <TextField
+            variant="outlined"
+            fullWidth
+            label="Enter your Password"
+            style={{ marginTop: "20px" }}
+            onChange={handleChangePassword}
+            type="password"
+          />
         </div>
-    );
+      </div>
+      <Button
+        variant="contained"
+        onClick={handleSubmit}
+        type="submit"
+        id="create_button"
+      >
+        Create Account
+      </Button>
+    </div>
+  );
 };
 
 export default CreateAccount;
