799b8bf6ec9e3f8eac56ff2c81100362e290aceb
Himanshu Yadav <57978421+himan07@users.noreply.github.com>
2023-06-01 11:35:47 +0000

    Update 4 files

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "../assets/css/ChatBot.css";
 import Header from "./Header";
+import video from "../assets/video/chatbot.mp4";
 
 const ChatBot = () => {
   return (
-    <div className="container">
-      <div className="chat_boat">
-        <Header />
-        <div className="chat_bot_field">
-          <input type="text" className="input_text" />
+    <>
+      <div className="container">
+        <video id="videoBackground" autoPlay loop muted>
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className="chat_boat">
+          <Header />
+          <div className="chat_bot_field">
+            <input type="text" className="input_text" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
