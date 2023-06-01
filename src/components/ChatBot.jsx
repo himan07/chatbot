24879b5c894fcb2f54b45659e78e868c4724e9c9
Himanshu Yadav <57978421+himan07@users.noreply.github.com>
2023-06-01 06:34:55 +0000

    chanfes

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 import "../assets/css/ChatBot.css";
-import Header from './Header';
+import Header from "./Header";
 
 const ChatBot = () => {
-    return (
-        <div className='container'>
-            <div className='chat_boat'>
-                <Header />
-                <dv className="chatbot_text">
-                    <h4>Hi I am Natasha How Can I help you?</h4>
-
-                </dv>
-                <div className='chat_bot_field'>
-                    <input type='text' className='input_text'/>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="chat_boat">
+        <Header />
+        {/* <dv className="chatbot_text">
+          <h4>Hi I am Natasha How Can I help you?</h4>
+        </dv> */}
+        <div className="chat_bot_field">
+          <input type="text" className="input_text" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ChatBot
+export default ChatBot;
